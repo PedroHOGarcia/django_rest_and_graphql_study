da614cdbcd11f378487d425ba59fc09a62a7449a
Pedro Garcia <eng.pedrohenriquegarcia@gmail.com>
2022-06-20 19:22:43 -0300

    Create employee model and test user creation.

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,19 @@
+from tabnanny import verbose
+from django import db
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique= True, db_index= True)
+    department = models.TextField()
+    name = models.TextField()
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural =  'Employees'
+    
+    def __str__(self):
+        return f'{self.name} works in {self.department}'
--- rest_api/tests.py
+++ rest_api/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse_lazy
 
 # Create your tests here.
+class ModelEmploeeTestCase(TestCase):
+    def setUp(self) -> None:
+        self.new_employee = {
+            'name': 'Pedro',
+            'department': 'IT',
+            'email': 'testcase@testcase.com'
+        }
+        self.client = Client()
+
+
+    def test_str_method(self):
+        self.assertTrue(str(self.employee), f'{self.employee.name} works in {self.employee.department}')
+
+    
+    def test_api_post_user_creation_view(self):
+        response = self.client.post(reverse_lazy('rest-api/v1/employee'), data= self.new_employee)
+        self.user_id = response.json()['result']['id']
+        self.assertTrue(response.status_code, 200)
+
+    
+    def test_api_get_user_from_view(self):
+        response = self.client.get(reverse_lazy(f'rest-api/v1/employee/{self.user_id}/'))
+        self.assertDictEqual(response['result'][0], self.new_employee)
+
+
+    def test_api_remove_user_view(self):
+        response = self.client.delete(reverse_lazy(f'rest-api/v1/employee/{self.user_id}'))
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_api_create_user_with_wrong_format(self):
+        local_dict = self.new_employee.copy()
+        local_dict.pop('name')
+
+        response = self.client.post(reverse_lazy('rest-api/v1/employee'), data= local_dict)
+        
+        self.assertFalse(response, 200)
