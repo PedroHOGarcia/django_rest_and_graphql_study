d6b43f280bcacfc5337eb241fa4439915179dbcb
Pedro Garcia <eng.pedrohenriquegarcia@gmail.com>
2022-06-20 19:56:14 -0300

    first migration and admin creation

---
+++ rest_api/serializers.py
@@ -0,0 +1,14 @@
+from dataclasses import fields
+from rest_framework import serializers
+
+from .models import Employee
+
+class EmployeeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Employee
+        fields = ['id', 
+                  'name', 
+                  'department', 
+                  'email']
+        
